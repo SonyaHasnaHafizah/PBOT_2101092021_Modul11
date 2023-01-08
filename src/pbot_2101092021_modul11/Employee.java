12b3ab7186be9c552f21c10a31854eb5a00af9f5
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2023-01-08 22:24:08 +0700

    Initial commit

---
+++ src/pbot_2101092021_modul11/Employee.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbot_2101092021_modul11;
+
+/**
+ *
+ * @author SONYA HASNA HAFIZAH
+ */
+public class Employee extends Person {
+    public String getName(){ 
+    System.out.println("Employee Name : " + name); 
+    return name; 
+    } 
+}
